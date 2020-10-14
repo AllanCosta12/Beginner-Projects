f91a70abb6df7a118514acb9f6d7507a1ddc6ad6
AllanCosta12 <72849591+AllanCosta12@users.noreply.github.com>
2020-10-14 18:10:49 -0300

    Update Tabuada.py

--- Tabuada.py
+++ Tabuada.py
@@ -1,7 +1,7 @@
-n = int(input('Digite qual a tubuada que deseja ->'))                   #Entrada e conversao do numero que iremos fazer a tabuada  /  Entry and conversion of the number that we will make the multiplication table
-nvzs = int(input('Digite ate que numero quer que va a tabuada ->'))        #informando ate que numero a tabuada vai  /  informing the number of times tables
+n = int(input('Digite qual a tubuada que deseja ->')) 
+nvzs = int(input('Digite ate que numero quer que va a tabuada ->'))        
 
-vzs = 0              #atribuindo 0 na variavel de controle  /  assigning 0 to the control variable
+vzs = 0             
 
-for vzs in range (nvzs+1):          #loop for
-    print (n,'X',vzs,'=', n*vzs)       #imprimindo texto e dados e o resultado  /  printing text and data and the result
+for vzs in range (nvzs+1):          
+    print (n,'X',vzs,'=', n*vzs)       
