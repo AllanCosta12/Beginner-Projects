2813f50b4a967565004003f4b61ca947c09d1c79
AllanCosta12 <72849591+AllanCosta12@users.noreply.github.com>
2020-10-14 18:12:30 -0300

    calculate average 3.0

---
+++ calcular_media3.0.py
@@ -0,0 +1,23 @@
+
+def calcular_media(n1,n2,n3,n4):
+    soma = n1+n2+n3+n4
+    media = soma / 4
+    return media
+def verifica_aprovado(media_calculada):
+    if media_calculada >= 8:
+        print('a media do aluno e', media_calculada, 'ele foi aprovado e esta de parabens')
+    elif media_calculada >= 6:
+        print('a media do aluno e', media_calculada, 'e ele foi aprovado')
+    else:
+        print('A media do aluno e', media_calculada, 'e ele foi reprovado')
+
+
+n1 = float(input('Digite a primeira nota ->'))
+n2 = float(input('Digite a segunda nota ->'))
+n3 = float(input('Digite a terceira nota ->'))
+n4 = float(input('Digite a quarta nota ->'))
+
+media_calculada = calcular_media(n1,n2,n3,n4)
+verifica_aprovado(media_calculada)
+
+
