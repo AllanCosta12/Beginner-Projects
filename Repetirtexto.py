ae88b855652df553985ca474e4f530a4108fdf56
AllanCosta12 <72849591+AllanCosta12@users.noreply.github.com>
2020-10-14 18:14:27 -0300

    Update Repetirtexto.py

--- Repetirtexto.py
+++ Repetirtexto.py
@@ -2,6 +2,6 @@ texto = input ('Digite oque deseja repetir ->')
 nvzs = int(input('Digite a quantidade de vezes que deseja que repita oque foi dito ->'))
 vzs = 0
 
-for vzs in range (nvzs):      #loop for
-     print (texto)            #Imprimindo texto  /  Printing text
+for vzs in range (nvzs):     
+     print (texto)            
 
