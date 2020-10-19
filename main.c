1e2e6688aec9318c2283897fcd8cef432fae2062
AllanCosta12 <72849591+AllanCosta12@users.noreply.github.com>
2020-10-19 00:42:39 -0300

    calculate average C

--- main.c
+++ main.c
@@ -3,57 +3,93 @@
 
 int main()
 {
-    int op, rep =1;
-    float x , y = 0;
+     int dec = 0;
+     float x,y,z,w,r,naluno1, naluno2 = 0;
 
+    printf("\t\t\tO que deseja fazer?\n\n");
+    printf (" 1- Calcular media de um aluno \n 2- Finalizar Programa\n");
+    scanf ("%i", &dec);
 
-        do{
-            printf ("\t\t\t\tTabuada\n");
-            printf (" 1- Soma \n 2- Subtracao \n 3- Multiplicacao \n 4- Divisao\n\n");
-            scanf ("%i", &op);
-
-    switch (op){
-
-            case 1:
-            printf ("\tDigite o primeiro numero a ser somado: ");
-            scanf ("%f", &x);
-            printf ("\tDigite o segundo numero a ser somado: ");
-            scanf ("%f", &y);
-            printf ("\n\t\t O resultado da soma dos numeros e: %.2f\n", x + y);
-            break;
-            case 2:
-            printf ("\tDigite o primeiro numero a ser subtraido: ");
-            scanf ("%f", &x);
-            printf ("\tDigite o segundo numero a ser subtraido: ");
-            scanf ("%f", &y);
-            printf ("\n\t\t O resultado da subtracao dos numeros e: %.2f\n", x - y);
-            break;
-            case 3:
-            printf ("\tDigite o primeiro numero a ser multiplicado: ");
-            scanf ("%f", &x);
-            printf ("\tDigite o segundo numero a ser multiplicado: ");
-            scanf ("%f", &y);
-            printf ("\n\t\t O resultado da multiplicacao dos numeros e: %.2f\n", x * y);
-            break;
-            case 4:
-            printf ("\tDigite o primeiro numero a ser dividido: ");
-            scanf ("%f", &x);
-            printf ("\tDigite o segundo numero a ser dividido: ");
-            scanf ("%f", &y);
-            printf ("\n\t\t O resultado da divisao dos numeros e: %.2f\n", x / y);
-            break;
-
-    default:
-                printf ("\t\t\tDigite um numero valido\n");
-        }
-        system ("pause");
+    switch (dec){
+    case 1:
         system("cls");
+        printf ("Digite a primeira nota do aluno:\n");
+        scanf ("%f", &x);
+        printf ("Digite a segunda nota do aluno:\n");
+        scanf ("%f", &y);
+        printf ("Digite a terceira nota do aluno:\n");
+        scanf ("%f", &z);
+        printf ("Digite a quarta nota do aluno:\n");
+        scanf ("%f", &w);
+        r = (x+y+z+w)/4;
 
-    printf ("\t\tDeseja fazer outra conta");
-    printf ("\n 1- SIM \n 2- NAO\n");
-    scanf ("%i", &rep);
-    system ("cls");
+            if (r >=5){
+                printf ("A media do aluno e: %.2f e ele foi aprovado", r);
+            }else{
+                printf ("A media do aluno e: %.2f e ele foi reprovado", r);
+            }
+         system("cls");
+         printf("\t\t\tO que deseja fazer?\n\n");
+         printf (" 1- Calcular media de um aluno \n 2- Comparar com outro aluno \n 3- Finalizar programa");
+         scanf ("%i", &dec);
 
-        }while (rep ==1);
+                switch (dec){
+                case 1:
+                        system("cls");
+                        printf ("Digite a primeira nota do aluno:\n");
+                        scanf ("%f", &x);
+                        printf ("Digite a segunda nota do aluno:\n");
+                        scanf ("%f", &y);
+                        printf ("Digite a terceira nota do aluno:\n");
+                        scanf ("%f", &z);
+                        printf ("Digite a quarta nota do aluno:\n");
+                        scanf ("%f", &w);
+                        r = (x+y+z+w)/4;
+
+                            if (r >=5){
+                                printf ("A media do aluno e: %.2f e ele foi aprovado", r);
+                                system ("pause");
+                            }else{
+                                printf ("A media do aluno e: %.2f e ele foi reprovado", r);
+                                system ("pause");
+                            }
+
+                         system("cls");
+                         printf("\t\t\tO que deseja fazer?\n\n");
+                         printf (" 1- Calcular media de um aluno \n 2- Comparar com outro aluno \n 3- Finalizar programa");
+                         scanf ("%i", &dec);
+                         break;
+                case 2:
+                        r = naluno1;
+                        system("cls");
+                        printf ("Digite a primeira nota do aluno:\n");
+                        scanf ("%f", &x);
+                        printf ("Digite a segunda nota do aluno:\n");
+                        scanf ("%f", &y);
+                        printf ("Digite a terceira nota do aluno:\n");
+                        scanf ("%f", &z);
+                        printf ("Digite a quarta nota do aluno:\n");
+                        scanf ("%f", &w);
+                        naluno2 = (x+y+z+w)/4;
+
+                            if (naluno1 > naluno2){
+                                printf("A nota do primeiro aluno e maior: %f",naluno1);
+                            }
+                             if (naluno1 < naluno2){
+                                printf("A nota do segundo aluno e maior: %f",naluno2);
+                            }
+                             if (naluno1 == naluno2){
+                                printf("Os alunos tiveram desempenho igual: %f",naluno1);
+                            }
+                            break;
+                case 3:
+                    break;
+                default:
+                    printf ("digite algo valido");
+
+                }
+    }
+
+
     return 0;
 }
